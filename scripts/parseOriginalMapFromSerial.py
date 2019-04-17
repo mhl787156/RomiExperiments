9a7709ca861047e75e3430430f3e610873508b65
Mickey LI Laptop <mickeyhli@outlook.com>
2019-04-17 20:18:15 +0100

    Updated python parse script

--- scripts/parseOriginalMapFromSerial.py
+++ scripts/parseOriginalMapFromSerial.py
@@ -1,26 +1,54 @@
 import serial
 import os
 import datetime
+import matplotlib.pyplot as plt
+import numpy
 
-PORT = "COM7"
-filename = "data/expr1-{}.map".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"))
+filePrepend = "Belief_Human_Readable"
 
-mymap = ""
-foundmap = False
-with serial.Serial('\\.\{}'.format(PORT)) as ser:
-    for i in range(1000):
-        raw = ser.readline()
-        decode = raw.decode('utf-8').strip()
-        if decode == 'Map':
-            if foundmap:
-                break
+PORT = "COM12"
+datafilename = "data/expr1-{}-{}.data".format(filePrepend, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"))
+mapfilename = "data/expr1-{}-{}.map".format(filePrepend, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"))
+
+# hl, = plt.plot([], [])
+
+# def update_plot(h1, new_data):
+#     hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
+#     hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
+#     plt.draw()
+
+# update_plot(hl, (900, 900))
+
+print("Script up finding ROMI on Bluetooth {}".format(PORT))
+mapString = ""
+dataString = ""
+with serial.Serial(PORT, timeout=3) as ser:
+    try:
+        while True:
+            current_time = datetime.datetime.now().strftime("%H-%M-%S")
+            raw = ser.readline()
+            decode = raw.decode('utf-8').strip()
+            print(current_time, decode if decode != "" else "TIMEOUT")
+
+            # Decode the map
+            if 'Map' in decode and 'Mapping' not in decode:
+                printstr = 'Map-{}\n'.format(current_time) 
+                mymap = printstr
+                for i in range(25):
+                    raw = ser.readline()
+                    decode = raw.decode('utf-8').strip()
+                    print(decode)
+                    mymap =  mymap + decode + '\n'
+                mapString = mapString + mymap + '\n'              
             else:
-                os.system('cls')
-                mymap = ""
-                foundmap = True
-        else:
-            print(decode)
-            mymap =  mymap + decode + '\n'
+                dataString = dataString + decode + '\n'               
+                
 
-with open(filename, 'w') as f:
-    f.write(mymap)
+    except KeyboardInterrupt:
+        print('Stopping and Saving')
+            
+
+with open(mapfilename, 'w+') as f:
+    f.write(mapString)
+with open(datafilename, 'w+') as f:
+    f.write(dataString)
